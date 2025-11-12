3849563581534bc30c79df308b7331709b93d08a
Danielle Lazzara <lazzaradani@gmail.com>
2025-11-12 18:32:19 -0500

    Fix duplicate content in portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,10 +1,6 @@
-import ForestNetwork from "./ForestNetwork";import ForestNetwork from "./ForestNetwork";
+import ForestNetwork from "./ForestNetwork";
 
-
-
-export default function PortfolioPage() {export default function Portfolio() {
-
-  return <ForestNetwork />;  return <ForestNetwork />;
-
-}}
+export default function PortfolioPage() {
+  return <ForestNetwork />;
+}
 
