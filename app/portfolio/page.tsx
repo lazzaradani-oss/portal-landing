a31a1138ea69ca57494869b25befab451dd4ba22
Danielle Lazzara <lazzaradani@gmail.com>
2025-11-12 18:31:23 -0500

    Add missing portfolio page route

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import ForestNetwork from "./ForestNetwork";import ForestNetwork from "./ForestNetwork";
+
+
+
+export default function PortfolioPage() {export default function Portfolio() {
+
+  return <ForestNetwork />;  return <ForestNetwork />;
+
+}}
+
